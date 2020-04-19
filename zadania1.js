dfb1ce14168d419f52775271bb51ba5f754a3009
Kasia Izak <kontakt@kasiaizak.pl>
2020-04-19 12:46:52 +0200

    add task 1

--- zadania1.js
+++ zadania1.js
@@ -1,26 +1,37 @@
 /** 1.
- * Zaimplementuj moduł Samochodu, który ma
- * A) -wewnętrzną (prywatną) właściwość - ilość benzyny (liczba)
- *    -wewnątrzną metodę spalania przjmującą za parametr ilość
- *      i albo ją odejmującą od ilości benzyny, albo wyświetlającą
- *      w konsoli wiadomość i pustym baku :(
- * 
- *  B) niech moduł zwraca obiekt z właściwością liczbową stacji radiowej,
- *     metodą by ją zmienić oraz drugą służącą do jechania, przyjmującą
- *     za parametr prędkość(liczba) i wykonywującą metodę z pkt. A 
- *     
- */
+  Zaimplementuj moduł Samochodu, który ma
+    A) - wewnętrzną (prywatną) właściwość - ilość benzyny (liczba)
+      - wewnątrzną metodę spalania przjmującą za parametr ilość i albo ją odejmującą od ilości benzyny, albo wyświetlającą w konsoli wiadomość i pustym baku :(
+  * B) niech moduł zwraca obiekt z właściwością liczbową stacji radiowej, metodą by ją zmienić oraz drugą służącą do jechania, przyjmującą za parametr prędkość(liczba) i wykonywującą metodę z pkt. A
+*/
 
- /**
-  *  Stwórz moduł - singleton. Niech posiada:
-  *
+ /** 2.
+  * Stwórz moduł - singleton. Niech posiada:
   * A) wewnętrzny stan - listę produków
-  * 
   * B) api:
-        publiczną metodę do dodawania produktu/ów
-        publiczną metodę do usuwania produktu/ów
-        publiczną metodę do wylistowania nazw produktów w stringu
-        publiczną metodę do usortowania produktów wg ceny
-        najważniejsze: niech pozwala na zsumowanie cen za produkty
+      - publiczną metodę do dodawania produktu/ów
+  - publiczną metodę do usuwania produktu/ów
+  - publiczną metodę do wylistowania nazw produktów w stringu
+  - publiczną metodę do usortowania produktów wg ceny
+  - najważniejsze: niech pozwala na zsumowanie cen za produkty
 
   */
+
+  function MyCar() {
+    var amountPetrol = 23;
+    function Combustion(i) {
+      if (amountPetrol - i > 0) {
+        console.log('Jedziemy')
+      } else {
+        console.log('Brak paliwa')
+      }
+    }
+    return {
+      Drive: function(speed) {
+        return Combustion(speed);
+      }
+    }
+  }
+
+  var car = MyCar()
+  car.Drive(3);
